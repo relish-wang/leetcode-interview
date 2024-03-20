d8b632480559851030535644edcdb5cbf0144d86
wangxin <wangxin21@corp.netease.com>
2024-03-20 10:19:49 +0800

    001 TwoSum

---
+++ src/main/kotlin/_001TwoSum.kt
@@ -0,0 +1,11 @@
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val size = nums.size
+    for (i in 0 until size - 1) {
+        for (j in i + 1 until size) {
+            if (nums[i] + nums[j] == target) {
+                return intArrayOf(i, j)
+            }
+        }
+    }
+    return intArrayOf(-1, -1)
+}
