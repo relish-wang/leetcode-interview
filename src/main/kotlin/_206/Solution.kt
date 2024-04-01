6e2ee9fdd5bd26ebf4e5ff66bebed703512e7aaf
wangxin21 <wangxin21@corp.netease.com>
2024-04-01 21:31:38 +0800

    206 ReverseList

---
+++ src/main/kotlin/_206/Solution.kt
@@ -0,0 +1,67 @@
+package _206
+
+import ListNode
+import java.util.*
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ * https://leetcode.cn/problems/reverse-linked-list/?envType=featured-list&envId=Fw9n57OM?envType=featured-list&envId=Fw9n57OM
+ */
+class Solution {
+    /**
+     * 题目要求迭代和递归各来一种解法, 这算迭代吧
+     */
+    fun reverseList1(head: ListNode?): ListNode? {
+        if (head == null) {
+            return null
+        }
+        val s = Stack<ListNode>()
+        var c = head
+        while (c != null) {
+            s.push(c)
+            c = c.next
+        }
+        val h = s.pop()
+        var pre = h
+        while (s.isNotEmpty()) {
+            val p = s.pop()
+            pre.next = p
+            p.next = null
+            pre = pre.next
+        }
+        return h
+    }
+    fun reverseList(head: ListNode?): ListNode? {
+        return reverseList(head, null)
+    }
+
+    /**
+     * [1,2,3,4,5] null
+     * [2,3,4,5] [1]
+     * [3,4,5] [2,1]
+     * [4,5] [3,2,1]
+     * [5] [4,3,2,1]
+     */
+    fun reverseList(head: ListNode?, tail: ListNode?): ListNode? {
+        if (head == null) {
+            return null
+        }
+        if (head.next == null) {
+            head.next = tail
+            return head
+        }
+        val next = head.next // 提前取出来
+        head.next = tail
+        return reverseList(next, head)
+    }
+}
+
+fun main() {
+    println(Solution().reverseList(ListNode.newInstance(intArrayOf(1, 2, 3, 4, 5))))
+}
