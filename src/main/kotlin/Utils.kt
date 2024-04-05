6026e43205373ab37b52689b46c84ccfbc75f2c6
relish-wang <relish.wang@gmail.com>
2024-04-05 12:47:59 +0800

    482 findMinArrowShots

---
+++ src/main/kotlin/Utils.kt
@@ -0,0 +1,14 @@
+fun int2d(raw: String): Array<IntArray> {
+    val arr = raw.substring(2, raw.length - 2).split(Regex("], *\\["))
+    val n = arr.size
+    return Array(n) { i ->
+        val s = arr[i]
+        if (s.isEmpty()) {
+            intArrayOf()
+        } else {
+            s.split(Regex(", *")).map {
+                it.trim().toInt()
+            }.toIntArray()
+        }
+    }
+}
