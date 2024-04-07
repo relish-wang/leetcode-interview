cc050ac9a430c9b9e8664cbcfae77ead99152dfa
wangxin21 <wangxin21@corp.netease.com>
2024-04-07 20:49:02 +0800

    _1115 foobar(synchronized + flag + wait()/notifyAll()).

--- src/main/kotlin/_1115/nolock/FooBar.java
+++ src/main/kotlin/_1115/nolock/FooBar.java
@@ -11,6 +11,16 @@ package _1115.nolock;
  * 7 wait() + notifyAll()
  * 【8】 无锁 // 打印5个会超时
  */
+/**
+ * https://leetcode.cn/problems/print-foobar-alternately/solutions/542996/duo-xian-cheng-liu-mai-shen-jian-ni-xue-d220n/?envType=featured-list&envId=Fw9n57OM?envType=featured-list&envId=Fw9n57OM
+ * 1 LinkedBlockingQueue
+ * 2 CyclicBarrier
+ * 3 自旋+Thread.yield() 让出cpu
+ * 4 可重入锁 ReentrantLock + Condition
+ * 5 synchronized + flag + wait()/notifyAll()
+ * 6 信号量 Semaphore
+ *【7】 无锁(仅flag) // 超时
+ */
 class FooBar {
     private int n;
     private volatile boolean canPrintBar = false;
--- src/main/kotlin/_1115/spin_yield/FooBar.java
+++ src/main/kotlin/_1115/spin_yield/FooBar.java
@@ -4,12 +4,11 @@ package _1115.spin_yield;
  * https://leetcode.cn/problems/print-foobar-alternately/solutions/542996/duo-xian-cheng-liu-mai-shen-jian-ni-xue-d220n/?envType=featured-list&envId=Fw9n57OM?envType=featured-list&envId=Fw9n57OM
  * 1 LinkedBlockingQueue
  * 2 CyclicBarrier
- * 【3】 自旋+Thread.yield() 让出cpu
+ *【3】 自旋+Thread.yield() 让出cpu
  * 4 可重入锁 ReentrantLock + Condition
- * 5 synchronized + 标志位 + 唤醒
+ * 5 synchronized + flag + wait()/notifyAll()
  * 6 信号量 Semaphore
- * 7 wait() + notifyAll()
- * 8 无锁
+ * 7 无锁(仅flag) // 超时
  */
 class FooBar {
     private int n;
