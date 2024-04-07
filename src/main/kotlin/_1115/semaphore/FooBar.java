c4b599d7617a321fd9eeb3b6c9d02d9e3b84db82
wangxin21 <wangxin21@corp.netease.com>
2024-04-07 21:26:30 +0800

    _1115 foobar(Semaphore)

--- src/main/kotlin/_1115/semaphore/FooBar.java
+++ src/main/kotlin/_1115/semaphore/FooBar.java
@@ -1,12 +1,15 @@
-package _1115;
+package _1115.semaphore;
+
+import java.util.concurrent.Semaphore;
+
 /**
  * https://leetcode.cn/problems/print-foobar-alternately/solutions/542996/duo-xian-cheng-liu-mai-shen-jian-ni-xue-d220n/?envType=featured-list&envId=Fw9n57OM?envType=featured-list&envId=Fw9n57OM
  * 1 LinkedBlockingQueue
  * 2 CyclicBarrier
  * 3 自旋+Thread.yield() 让出cpu
  * 4 可重入锁 ReentrantLock + Condition
- * 5 synchronized + 标志位 + 唤醒
- * 6 信号量 Semaphore
+ * 5 synchronized + flag + wait()/notifyAll()
+ * 【6】 信号量 Semaphore
  * 7 无锁(仅flag) // 超时
  */
 class FooBar {
@@ -16,35 +19,39 @@ class FooBar {
         this.n = n;
     }
 
+    private final Semaphore foo = new Semaphore(1);
+    private final Semaphore bar = new Semaphore(0);
+
     public void foo(Runnable printFoo) throws InterruptedException {
-        
+
         for (int i = 0; i < n; i++) {
-            
-        	// printFoo.run() outputs "foo". Do not change or remove this line.
-        	printFoo.run();
+            foo.acquire();
+            printFoo.run();
+            bar.release();
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
-        
+
         for (int i = 0; i < n; i++) {
-            
-            // printBar.run() outputs "bar". Do not change or remove this line.
-        	printBar.run();
+            bar.acquire();
+            printBar.run();
+            foo.release();
         }
     }
-    public static void main(String[] args) throws RuntimeException{
+
+    public static void main(String[] args) throws RuntimeException {
         final FooBar fooBar = new FooBar(4);
         new Thread(() -> {
             try {
-                fooBar.bar(()-> System.out.print("bar"));
+                fooBar.bar(() -> System.out.print("bar"));
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }).start();
         new Thread(() -> {
             try {
-                fooBar.foo(()-> System.out.print("foo"));
+                fooBar.foo(() -> System.out.print("foo"));
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
