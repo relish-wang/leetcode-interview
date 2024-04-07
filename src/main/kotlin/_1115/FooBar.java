26c5112c4ac203b873fc81438cd233a007d46378
wangxin21 <wangxin21@corp.netease.com>
2024-04-07 20:32:13 +0800

    _1115 foobar(ReentrantLock + Condition).

--- src/main/kotlin/_1115/FooBar.java
+++ src/main/kotlin/_1115/FooBar.java
@@ -3,7 +3,7 @@ package _1115;
  * https://leetcode.cn/problems/print-foobar-alternately/solutions/542996/duo-xian-cheng-liu-mai-shen-jian-ni-xue-d220n/?envType=featured-list&envId=Fw9n57OM?envType=featured-list&envId=Fw9n57OM
  * 1 LinkedBlockingQueue
  * 2 CyclicBarrier
- * 3 自旋+yield
+ * 3 自旋+Thread.yield() 让出cpu
  * 4 可重入锁 ReentrantLock + Condition
  * 5 synchronized + 标志位 + 唤醒
  * 6 信号量 Semaphore
