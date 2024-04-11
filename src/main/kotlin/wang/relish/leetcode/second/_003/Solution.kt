ed69300781182938c579729f2dd1d0a338689dd2
wangxin21 <wangxin21@corp.netease.com>
2024-04-11 20:12:18 +0800

    second: 003 with time

--- src/main/kotlin/wang/relish/leetcode/second/_003/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_003/Solution.kt
@@ -1,43 +1,37 @@
 package wang.relish.leetcode.second._003
 
 import kotlin.math.max
-import kotlin.math.min
 
 class Solution {
-    /**
-     * 滑动窗口
-     * 用上set就够了左指针右移删一个set.remove(s[l])
-     * 用上set就够了右指针右移加一个set.add(s[r])
-     */
     fun lengthOfLongestSubstring(s: String): Int {
         val n = s.length
-        if (n == 0) {
-            return 0
+        if (n <= 1) {
+            return n
         }
-        var max = 1
         var l = 0
         var r = 1
-        val map = linkedMapOf<Char, Int>()
+        var max = 1
+        val map = hashMapOf<Char, Int>()
         map[s[0]] = 0
-        while (r in l..<n) {
-            while (r < n && map.contains(s[r]).not()) {
-                map[s[r]] = r
-                r++
+        while (l <= r && r < n) {
+            if (map.contains(s[r])) {
+                max = max(r - l, max)
+                val index = map[s[r]]!!
+                for (i in l..index) {
+                    map.remove(s[i])
+                }
+                l = index + 1
             }
-            max = max(r - l, max)
-            val end = min(map[s[min(r, n - 1)]]!! + 1, n - 1)
-            for (i in l until end) {
-                map.remove(s[i])
-            }
-            l = end
+            map[s[r]] = r
+            r++
         }
+        max = max(r - l, max)
         return max
     }
 }
 
 fun main() {
-    println(Solution().lengthOfLongestSubstring("abcabcbb")) //3
-    println(Solution().lengthOfLongestSubstring("dvdf")) // 3
-    println(Solution().lengthOfLongestSubstring("pwwkew")) // 3
-    println(Solution().lengthOfLongestSubstring("bbbbb")) // 1
+//    println(Solution().lengthOfLongestSubstring("abcabcbb"))//3
+//    println(Solution().lengthOfLongestSubstring("pwwkew"))//3
+    println(Solution().lengthOfLongestSubstring("au"))//2
 }
