507d75f8f936055fc0f850b0bb143bceefe80bc5
wangxin21 <wangxin21@corp.netease.com>
2024-04-11 21:18:23 +0800

    second: 019 with time.

--- src/main/kotlin/wang/relish/leetcode/second/_019/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_019/Solution.kt
@@ -2,40 +2,29 @@ package wang.relish.leetcode.second._019
 
 import struct.ListNode
 
-/**
- * Example:
- * var li = ListNode(5)
- * var v = li.`val`
- * Definition for singly-linked list.
- * class ListNode(var `val`: Int) {
- *     var next: ListNode? = null
- * }
- */
 class Solution {
     fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
         if (head == null) {
             return null
         }
-        val dummy = ListNode(-1).apply { next = head }
-        var fast: ListNode? = dummy
-        var slow: ListNode? = dummy
+        var d = ListNode(0).apply { next = head }
+        var slow: ListNode? = d
+        var fast: ListNode? = d
         var i = 0
-        while (i < n && fast != null) {
-            i++
+        while (fast != null) {
             fast = fast.next
+            i++
+            if (i == n) {
+                break
+            }
         }
-        var pre = dummy
+        var pre: ListNode? = null
         while (slow != null && fast != null) {
             pre = slow
             slow = slow.next
             fast = fast.next
         }
-        pre.next = slow?.next
-        return dummy.next
+        pre?.next = slow?.next
+        return d.next
     }
-}
-
-fun main() {
-//    println(Solution().removeNthFromEnd(ListNode.newInstance(intArrayOf(1, 2, 3, 4, 5)), 2))
-    println(Solution().removeNthFromEnd(ListNode.newInstance(intArrayOf(1)), 1))
 }
