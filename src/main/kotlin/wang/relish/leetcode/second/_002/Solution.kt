fbc77dab714c5358d3b59cc6153b9926205398bf
wangxin21 <wangxin21@corp.netease.com>
2024-04-11 19:50:54 +0800

    second: 002 with time

--- src/main/kotlin/wang/relish/leetcode/second/_002/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_002/Solution.kt
@@ -2,51 +2,50 @@ package wang.relish.leetcode.second._002
 
 import struct.ListNode
 
-/**
- * Example:
- * var li = ListNode(5)
- * var v = li.`val`
- * Definition for singly-linked list.
- * class ListNode(var `val`: Int) {
- *     var next: ListNode? = null
- * }
- */
 class Solution {
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-        if (l1 == null || l2 == null) return null
-        var p1 = l1
-        var p2 = l2
-        var head: ListNode? = null
-        var c: ListNode? = null
-        var m = 0
-        while (p1 != null || p2 != null) {
-            val v1 = p1?.`val` ?: 0
-            val v2 = p2?.`val` ?: 0
-            val t = v1 + v2 + m
-            m = t / 10
-            val v = t % 10
-            if (head == null || c == null) {
-                head = ListNode(v)
-                c = head
+        if (l1 == null) return l2
+        if (l2 == null) return l1
+        val dummy = ListNode(0)
+        var cur: ListNode? = dummy
+        var n1 = l1
+        var n2 = l2
+        var c = 0
+        while (n1 != null || n2 != null) {
+            val v1 = n1?.`val` ?: 0
+            val v2 = n2?.`val` ?: 0
+            val v = v1 + v2 + c
+            c = v / 10
+            cur?.next = ListNode(v % 10)
+            cur = cur?.next
+            n1 = n1?.next
+            n2 = n2?.next
+        }
+        if (c > 0) {
+            if (n1 != null) {
+                cur?.next = ListNode(n1.`val` + c)
+                cur = cur?.next
+                n1 = n1.next
+                cur?.next = n1
+            } else if (n2 != null) {
+                cur?.next = ListNode(n2.`val` + c)
+                cur = cur?.next
+                n2 = n2.next
+                cur?.next = n2
             } else {
-                c.next = ListNode(v)
-                c = c.next
+                cur?.next = ListNode(c)
             }
-            p1 = p1?.next
-            p2 = p2?.next
+        } else {
+            if (n1 != null) {
+                cur?.next = n1
+            } else if (n2 != null) {
+                cur?.next = n2
+            }
         }
-        if (m > 0) {
-            c?.next = ListNode(m)
-        }
-        return head
+        return dummy.next
     }
 }
 
 fun main() {
-    println(
-        Solution().addTwoNumbers(
-            ListNode.newInstance(intArrayOf(2, 4, 3)),
-            ListNode.newInstance(intArrayOf(5, 6, 4))
-        )
-    )
+
 }
