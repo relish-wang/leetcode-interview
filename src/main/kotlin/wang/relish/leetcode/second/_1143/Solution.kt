8079d51311b5579b6d2e991d292d8953f9ceb503
wangxin21 <wangxin21@corp.netease.com>
2024-04-13 01:59:05 +0800

    second: 1143 update record

--- src/main/kotlin/wang/relish/leetcode/second/_1143/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_1143/Solution.kt
@@ -1,5 +1,7 @@
 package wang.relish.leetcode.second._1143
 
+import kotlin.math.max
+
 
 /**
  * https://leetcode.cn/problems/longest-common-subsequence/description/
@@ -7,7 +9,22 @@ package wang.relish.leetcode.second._1143
 class Solution {
 
     fun longestCommonSubsequence(text1: String, text2: String): Int {
-        return 0
+        val l1 = text1.length
+        val l2 = text2.length
+        if (l1 == 0 || l2 == 0) return 0
+        val dp = Array(l1 + 1) { IntArray(l2 + 1) { 0 } }
+        var max = 0
+        for (i in 1..l1) {
+            for (j in 1..l2) {
+                dp[i][j] = if (text1[i - 1] == text2[j - 1]) {
+                    dp[i - 1][j - 1] + 1
+                } else {
+                    max(max(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1])
+                }
+                max = max(max, dp[i][j])
+            }
+        }
+        return max
     }
 }
 
@@ -15,4 +32,5 @@ fun main() {
     println(Solution().longestCommonSubsequence("abcde", "ace")) // 3 ace
     println(Solution().longestCommonSubsequence("abc", "abc")) // 3
     println(Solution().longestCommonSubsequence("abc", "def")) // 0
+    println(Solution().longestCommonSubsequence("psnw", "vozsh")) // 1
 }
