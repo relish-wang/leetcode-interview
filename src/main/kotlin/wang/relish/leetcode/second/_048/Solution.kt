4066e8e62f7112a49465360c4263ac4350fa4c86
wangxin21 <wangxin21@corp.netease.com>
2024-04-12 19:27:17 +0800

    second: 048 update record.

--- src/main/kotlin/wang/relish/leetcode/second/_048/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_048/Solution.kt
@@ -6,26 +6,19 @@ import struct.printSquare
 class Solution {
     fun rotate(matrix: Array<IntArray>) {
         val n = matrix.size
-        val maxLevel = n / 2
-        for (l in 0..maxLevel) {
-            f(matrix, l)
+        for (i in 0 until n / 2) {
+            f(matrix, i)
         }
     }
 
-    fun f(matrix: Array<IntArray>, level: Int) {
-        val n = matrix.size - level * 2
-        // 4个数互换
+    fun f(a: Array<IntArray>, d: Int) {
+        val n = a.size - d * 2
         for (i in 0 until n - 1) {
-            val t = matrix[level][level + i]
-            // 第一个数
-            matrix[level][level + i] = matrix[level + n - 1 - i][level]
-            // 第二个数
-            matrix[level + n - 1 - i][level] = matrix[level + n - 1][n - 1 - i + level]
-            // 第三个数
-            matrix[level + n - 1][n - 1 - i + level] = matrix[level + i][level + n - 1]
-            // 第四个数
-            matrix[level + i][level + n - 1] = t
-            println()
+            val t = a[d][d + i]
+            a[d][d + i] = a[d + n - 1 - i][d]
+            a[d + n - 1 - i][d] = a[d + n - 1][d + n - 1 - i]
+            a[d + n - 1][d + n - 1 - i] = a[d + i][d + n - 1]
+            a[d + i][d + n - 1] = t
         }
     }
 }
