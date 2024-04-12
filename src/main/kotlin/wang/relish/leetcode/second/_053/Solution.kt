4a810aa4a206ddbc792149ac635f97ebb3db8abd
wangxin21 <wangxin21@corp.netease.com>
2024-04-13 01:05:32 +0800

    second: 053 update record.

--- src/main/kotlin/wang/relish/leetcode/second/_053/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_053/Solution.kt
@@ -2,17 +2,17 @@ package wang.relish.leetcode.second._053
 
 import kotlin.math.max
 
+
 class Solution {
 
     fun maxSubArray(nums: IntArray): Int {
         val n = nums.size
-        if (n == 0) return 0
-        val dp = IntArray(n) { 0 }
-        dp[0] = nums[0]
-        var max = dp[0]
-        for (i in 1 until n) {
-            dp[i] = max(dp[i - 1] + nums[i], nums[i])
-            max = max(dp[i], max)
+        if(n==0)return 0
+        var max = nums[0]
+        var pre = max
+        for (i in 1 until n){
+            pre = max(pre+nums[i], nums[i])
+            max = max(pre, max)
         }
         return max
     }
