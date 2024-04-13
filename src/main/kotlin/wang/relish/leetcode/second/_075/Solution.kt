f73dbf5227dc4841c59722c025604b0a6ee7b57a
wangxin21 <wangxin21@corp.netease.com>
2024-04-13 11:55:07 +0800

    second: 075

--- src/main/kotlin/wang/relish/leetcode/second/_075/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_075/Solution.kt
@@ -2,23 +2,40 @@ package wang.relish.leetcode.second._075
 
 class Solution {
     fun sortColors(nums: IntArray): Unit {
-        var count0 = 0
-        var count1 = 0
-        for (i in 0 until nums.size) {
+        val n = nums.size
+        var p0 = 0
+        var p1 = 0
+        for (i in 0 until n) {
             if (nums[i] == 0) {
-                count0++
+                swap(nums, i, p0)
+                if (nums[i] == 1) {
+                    swap(nums, i, p1)
+                }
+                p0++
+                p1++
             } else if (nums[i] == 1) {
-                count1++
-            }
-        }
-        for (i in 0 until nums.size) {
-            if (i < count0) {
-                nums[i] = 0
-            } else if (i < count0 + count1) {
-                nums[i] = 1
-            } else {
-                nums[i] = 2
+                swap(nums, i, p1)
+                p1++
             }
         }
     }
+
+    fun swap(a: IntArray, i: Int, j: Int) {
+        val t = a[i]
+        a[i] = a[j]
+        a[j] = t
+    }
+}
+
+fun main() {
+    val nums = intArrayOf(2, 0, 2, 1, 1, 0)
+//    val nums = intArrayOf(2, 0, 1)
+//    val nums = intArrayOf(2, 1, 2)
+//    val nums = intArrayOf(2)
+//    val nums = intArrayOf(0, 2)
+//    val nums = intArrayOf(0, 2, 2, 2, 0, 2, 1, 1)
+
+    println(nums.contentToString())
+    Solution().sortColors(nums)
+    println(nums.contentToString())
 }
