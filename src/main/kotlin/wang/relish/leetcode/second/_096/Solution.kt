943c307d4024bff552d48c8bd3d57ed01f3e165d
relish-wang <relish.wang@gmail.com>
2024-04-20 07:39:18 +0800

    second: 096 update record.

--- src/main/kotlin/wang/relish/leetcode/second/_096/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_096/Solution.kt
@@ -3,32 +3,32 @@ package wang.relish.leetcode.second._096
 class Solution {
     /**
      * 二叉搜索树的梳理(节点数值为[1-n])
-     *
-     * ans = ans * 2 * (2 * i + 1) / (i + 2) // i属于[0,n)
      */
-    fun numTrees(n: Int): Int {
+    fun numTrees1(n: Int): Int {
+        if (n <= 1) {
+            return 1
+        }
         val dp = IntArray(n + 1) { 0 }
         dp[0] = 1
         dp[1] = 1
         for (i in 2..n) {
             for (j in 1..i) {
-                dp[i] += dp[j - 1] * dp[i - j]
+                dp[i] += dp[i - j] * dp[j - 1]
             }
         }
         return dp[n]
     }
 
-    class Solution {
-        /**
-         * 最优, 卡特兰数
-         */
-        fun numTrees(n: Int): Int {
-            var ans = 1L
-            for (i in 0 until n) {
-                ans = ans * 2 * (2 * i + 1) / (i + 2)
-            }
-            return ans.toInt()
+    /**
+     * 最优, 卡特兰数
+     * ans = ans * 2 * (2 * i + 1) / (i + 2) // i属于[0,n)
+     */
+    fun numTrees(n: Int): Int {
+        var ans = 1
+        for (i in 1 until n) {
+            ans = ans * 2 * (2 * i + 1) / (i + 2)
         }
+        return ans
     }
 }
 
