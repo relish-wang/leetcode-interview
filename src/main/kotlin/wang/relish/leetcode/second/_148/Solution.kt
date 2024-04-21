8b4330b4780800a02405b25a497d4801149ee4ae
relish-wang <relish.wang@gmail.com>
2024-04-21 17:00:36 +0800

    second: 148 with empty.

---
+++ src/main/kotlin/wang/relish/leetcode/second/_148/Solution.kt
@@ -0,0 +1,24 @@
+package wang.relish.leetcode.second._148
+
+import struct.ListNode
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun sortList(head: ListNode?): ListNode? {
+
+    }
+}
+
+fun main() {
+    val head = ListNode.newInstance(intArrayOf(4, 19, 14, 5, -3, 1, 8, 5, 11, 15))
+    println(head)
+    println(Solution().sortList(head))
+}
