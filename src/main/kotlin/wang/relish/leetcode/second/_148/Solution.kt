7a558fdd94606f6e4b4c5be2775cfc9e0986c0c0
wangxin <wangxin21@corp.netease.com>
2024-04-22 11:07:09 +0800

    second: 148 update record.

--- src/main/kotlin/wang/relish/leetcode/second/_148/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_148/Solution.kt
@@ -13,7 +13,62 @@ import struct.ListNode
  */
 class Solution {
     fun sortList(head: ListNode?): ListNode? {
-        return null
+        if (head?.next == null) {
+            return head
+        }
+        val (l1, l2) = cut(head)
+        val newL1 = sortList(l1)
+        val newL2 = sortList(l2)
+        return merge(newL1, newL2)
+    }
+
+    fun cut(head: ListNode?): Pair<ListNode?, ListNode?> {
+        if (head?.next == null) {
+            return head to null
+        }
+        var slow = head
+        var fast = head.next
+        var c = 0
+        while (fast?.next != null) {
+            slow = slow?.next
+            fast = fast.next?.next
+            c++
+        }
+        val next = slow?.next
+        slow?.next = null
+        return head to next
+    }
+
+    fun len(n: ListNode?): Int {
+        var c = n
+        var co = 0
+        while (c != null) {
+            co++
+            c = c.next
+        }
+        return co
+    }
+
+    fun merge(n1: ListNode?, n2: ListNode?): ListNode? {
+        if (n1 == null) return n2
+        if (n2 == null) return n1
+        val dummy = ListNode(0)
+        var c = dummy
+        var l1 = n1
+        var l2 = n2
+        while (l1 != null && l2 != null) {
+            if (l1.`val` < l2.`val`) {
+                c.next = l1
+                l1 = l1.next
+
+            } else {
+                c.next = l2
+                l2 = l2.next
+            }
+            c = c.next!!
+        }
+        c.next = l1 ?: l2
+        return dummy.next
     }
 }
 
