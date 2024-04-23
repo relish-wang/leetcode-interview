578c44c0fbf1b0d6c23f91583ddbf129fd33ad71
wangxin <wangxin21@corp.netease.com>
2024-04-23 14:41:07 +0800

    second: 200 update record.

--- src/main/kotlin/wang/relish/leetcode/second/_0200/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_0200/Solution.kt
@@ -1,11 +1,38 @@
-package wang.relish.leetcode._0200
+package wang.relish.leetcode.second._0200
 
 /**
  * https://leetcode.cn/problems/number-of-islands/?envType=featured-list&envId=Fw9n57OM?envType=featured-list&envId=Fw9n57OM
  */
 class Solution {
     fun numIslands(grid: Array<CharArray>): Int {
-        return 0
+        val m = grid.size
+        val n = grid[0].size
+        var ans = 0
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j)
+                    ans++
+                }
+            }
+        }
+        return ans
+    }
+
+    fun dfs(grid: Array<CharArray>, i: Int, j: Int) {
+        val m = grid.size
+        val n = grid[0].size
+        if (i !in 0 until m || j !in 0 until n) {
+            return
+        }
+        if (grid[i][j] != '1') {
+            return
+        }
+        grid[i][j] = '2'
+        dfs(grid, i - 1, j)
+        dfs(grid, i, j - 1)
+        dfs(grid, i + 1, j)
+        dfs(grid, i, j + 1)
     }
 }
 
