c22519cca2b4bc7c81cee2df38790cd29f117d1a
wangxin21 <wangxin21@corp.netease.com>
2024-04-23 20:54:00 +0800

    second: 0236 update record.

--- src/main/kotlin/wang/relish/leetcode/second/_0236/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_0236/Solution.kt
@@ -18,7 +18,33 @@ class Solution {
      * 内存: 83.10%
      */
     fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+        if (root == null) return null
+        if (p == null || find(q, p.`val`)) return q
+        if (q == null || find(p, q.`val`)) return p
+        val pv = p.`val`
+        val qv = q.`val`
+        var c = root
+        while (c != null) {
+            val l = find(c.left, pv) && find(c.left, qv)
+            if (l) {
+                c = c.left
+                continue
+            }
+            val r = find(c.right, pv) && find(c.right, qv)
+            if (r) {
+                c = c.right
+                continue
+            }
+            return c
+        }
         return null
+    }
+    fun find(a: TreeNode?, k: Int): Boolean {
+        if (a == null) return false
+        if (a.`val` == k) {
+            return true
+        }
+        return find(a.left, k) || find(a.right, k)
     }
 }
 
@@ -28,5 +54,5 @@ fun main() {
     root?.print()
     val p = root?.findChildTree(8)
     val q = root?.findChildTree(4)
-    println("${p?.`val`} 和 ${q?.`val`} 的最近公共父节点是: " + Solution().lowestCommonAncestor(root, p, q)?.`val`)
+    println("${p?.`val`} 和 ${q?.`val`} 的最近公共父节点是: " + Solution().lowestCommonAncestor(root, p, q)?.`val`) // 0
 }
