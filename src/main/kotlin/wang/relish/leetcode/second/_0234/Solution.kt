a2b3fba937e6e528bd674f493827b6e916e1370c
wangxin <wangxin21@corp.netease.com>
2024-04-23 18:19:35 +0800

    second: 0234 update record.

--- src/main/kotlin/wang/relish/leetcode/second/_0234/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_0234/Solution.kt
@@ -16,11 +16,39 @@ class Solution {
      * 时间O(n), 空间O(1)
      */
     fun isPalindrome(head: ListNode?): Boolean {
-        return false
+        var slow = head
+        var fast = head
+        while (fast?.next?.next != null) {
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+        var p1 = head
+        var p2 = reverse(slow?.next)
+        while (p2 != null) {
+            if (p1?.`val` != p2.`val`) {
+                return false
+            }
+            p1 = p1.next
+            p2 = p2.next
+        }
+        return true
+    }
+
+    fun reverse(head: ListNode?): ListNode? {
+        var pre: ListNode? = null
+        var cur = head
+        while (cur != null) {
+            val t = cur.next
+            cur.next = pre
+            pre = cur
+            cur = t
+        }
+        return pre
     }
 }
 
 fun main() {
-    println(Solution().isPalindrome(ListNode.newInstance(intArrayOf(1, 2, 2, 1))))
-    println(Solution().isPalindrome(ListNode.newInstance(intArrayOf(1, 2, 2, 3, 1))))
+    println(Solution().isPalindrome(ListNode.newInstance(intArrayOf(1, 2, 2, 1)))) //  true
+    println(Solution().isPalindrome(ListNode.newInstance(intArrayOf(1, 2, 2, 3, 1)))) // false
+    println(Solution().isPalindrome(ListNode.newInstance(intArrayOf(1, 2, 2, 2, 1)))) // true
 }
