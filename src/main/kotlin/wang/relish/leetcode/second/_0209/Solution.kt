a8c0f0e59a765abd986be2a1a4a20452b193f848
wangxin <wangxin21@corp.netease.com>
2024-04-23 16:13:56 +0800

    second: 209 update record.

--- src/main/kotlin/wang/relish/leetcode/second/_0209/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_0209/Solution.kt
@@ -1,12 +1,15 @@
 package wang.relish.leetcode.second._0209
 
+import java.util.*
 import kotlin.math.min
 
 class Solution {
     /**
      * [jump](https://leetcode.cn/problems/minimum-size-subarray-sum/?envType=featured-list&envId=Fw9n57OM?envType=featured-list&envId=Fw9n57OM)
+     * 滑动窗口, 时间O(n), 空间(1)。
+     * 但进阶要求时间复杂度O(n log(n))。啊？劣化性能？
      */
-    fun minSubArrayLen(target: Int, nums: IntArray): Int {
+    fun minSubArrayLen1(target: Int, nums: IntArray): Int {
         var l = 0
         var r = 1
         var win = nums[0]
@@ -38,7 +41,28 @@ class Solution {
             able = true
             ans = min(ans, r - l)
         }
+        Arrays.binarySearch(nums, 10)
         return if (able) ans else 0
+    }
+
+    fun minSubArrayLen(target: Int, nums: IntArray): Int {
+        val n = nums.size
+        var ans = Int.MAX_VALUE
+        val sum = IntArray(n + 1) { 0 }
+        for (i in 1..n) {
+            sum[i] = sum[i - 1] + nums[i - 1]
+        }
+        for (i in 1..n) {
+            val t = target + sum[i - 1]
+            var bound = Arrays.binarySearch(sum, t)
+            if (bound < 0) { // 没找到返回了-(low+1)
+                bound = -bound - 1
+            }
+            if (bound <= n) {
+                ans = min(ans, bound - i + 1)
+            }
+        }
+        return ans.takeIf { it != Int.MAX_VALUE } ?: 0
     }
 }
 
