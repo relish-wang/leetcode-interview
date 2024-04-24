e078688ce8aa4dd5af8dbee0a0dedbfe31f29f34
wangxin <wangxin21@corp.netease.com>
2024-04-24 17:47:56 +0800

    second: 0331 update record.

--- src/main/kotlin/wang/relish/leetcode/second/_0331/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_0331/Solution.kt
@@ -5,13 +5,34 @@ package wang.relish.leetcode.second._0331
  */
 class Solution {
     fun isValidSerialization(preorder: String): Boolean {
-        return false
+        if (preorder == "#") return true
+        val arr = ArrayList(preorder.split(","))
+        while (arr.size > 1) {
+            val n = arr.size
+            if (n >= 3) {
+                var i = n - 1
+                while (i >= 0 && arr[i] == "#") {
+                    i--
+                }
+                if (i < 0) return false
+                if (arr[i] != "#" && i <= n - 3) {
+                    arr[i] = "#"
+                    arr.removeAt(i)
+                    arr.removeAt(i)
+                } else {
+                    return false
+                }
+            } else {
+                return false
+            }
+        }
+        return arr.size == 1 && arr[0] == "#"
     }
 }
 
 fun main() {
-//    println(Solution().isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#")) // true
-//    println(Solution().isValidSerialization("1,#")) // false
-//    println(Solution().isValidSerialization("9,#,#,1")) // false
+    println(Solution().isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#")) // true
+    println(Solution().isValidSerialization("1,#")) // false
+    println(Solution().isValidSerialization("9,#,#,1")) // false
     println(Solution().isValidSerialization("1,#,#,#,#")) // false
 }
