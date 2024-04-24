8e52e97593bbda2ca245360198c07d0f02cadb36
wangxin21 <wangxin21@corp.netease.com>
2024-04-24 20:37:09 +0800

    second: 0452 update record.

--- src/main/kotlin/wang/relish/leetcode/second/_0452/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_0452/Solution.kt
@@ -1,7 +1,6 @@
-package wang.relish.leetcode.second._0482
+package wang.relish.leetcode.second._0452
 
 import struct.int2d
-import java.util.*
 
 /**
  * https://leetcode.cn/problems/minimum-number-of-arrows-to-burst-balloons/?envType=featured-list&envId=Fw9n57OM?envType=featured-list&envId=Fw9n57OM
@@ -18,7 +17,36 @@ class Solution {
      *
      */
     fun findMinArrowShots(points: Array<IntArray>): Int {
-        return 0
+        points.sortWith(object : Comparator<IntArray> {
+            override fun compare(p0: IntArray?, p1: IntArray?): Int {
+                if (p0 == null || p1 == null) return 0
+                return if (p0[1] > p1[1]) {
+                    1
+                } else if (p0[1] == p1[1]) {
+                    if (p0[0] > p1[0]) {
+                        1
+                    } else if (p0[0] == p1[0]) {
+                        0
+                    } else {
+                        -1
+                    }
+                } else {
+                    -1
+                }
+            }
+        })
+        var right = points[0][1]
+        var ans = 1
+        for (i in 1 until points.size) {
+            if(points[i].inArea(right)) continue
+            ans++
+            right = points[i][1]
+        }
+        return ans
+    }
+
+    fun IntArray.inArea(index:Int):Boolean{
+        return index in this[0]..this[1]
     }
 }
 
