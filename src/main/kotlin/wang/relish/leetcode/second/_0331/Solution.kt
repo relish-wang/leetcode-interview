cc91325e1dd020bb3807296b3924e9d4a47a3d8f
wangxin <wangxin21@corp.netease.com>
2024-04-24 18:02:36 +0800

    second: 0331 by counter solution without 'split()' update record.

--- src/main/kotlin/wang/relish/leetcode/second/_0331/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_0331/Solution.kt
@@ -4,7 +4,55 @@ package wang.relish.leetcode.second._0331
  * https://leetcode.cn/problems/verify-preorder-serialization-of-a-binary-tree/?envType=featured-list&envId=Fw9n57OM?envType=featured-list&envId=Fw9n57OM
  */
 class Solution {
+
+    /**
+     * 计数法(不用split)
+     * 03:21
+     */
     fun isValidSerialization(preorder: String): Boolean {
+        if (preorder == "#") return true
+        val n = preorder.length
+        var i = 0
+        var c = 1
+        while (i < n && c > 0) {
+            if (preorder[i] == ',') {
+                i++
+                continue
+            }
+            if (preorder[i] == '#') {
+                c--
+                i++
+                continue
+            }
+            while (i < n && preorder[i] != ',') i++
+            if (i == n) return false
+            c++
+            i++
+        }
+        return c == 0 && i == n
+    }
+
+    /**
+     * 计数法
+     * 2:09
+     */
+    fun isValidSerialization2(preorder: String): Boolean {
+        if (preorder == "#") return true
+        val arr = preorder.split(",")
+        val n = arr.size
+        var c = 1
+        var i = 0
+        while (i < n && c > 0) {
+            if (arr[i++] == "#") {
+                c--
+            } else {
+                c++
+            }
+        }
+        return i == n && c == 0
+    }
+
+    fun isValidSerialization1(preorder: String): Boolean {
         if (preorder == "#") return true
         val arr = ArrayList(preorder.split(","))
         while (arr.size > 1) {
