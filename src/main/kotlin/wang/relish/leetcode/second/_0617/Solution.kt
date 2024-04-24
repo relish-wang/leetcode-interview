d3938cfd6c33e09edf56e932b1781b1f24a2ba89
wangxin21 <wangxin21@corp.netease.com>
2024-04-24 20:43:11 +0800

    second: 0617 update record.

--- src/main/kotlin/wang/relish/leetcode/second/_0617/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_0617/Solution.kt
@@ -14,12 +14,17 @@ import struct.TreeNode
  */
 class Solution {
     fun mergeTrees(root1: TreeNode?, root2: TreeNode?): TreeNode? {
-        return null
+        if (root1 == null) return root2
+        if (root2 == null) return root1
+        val root = TreeNode(root1.`val` + root2.`val`)
+        root.left = mergeTrees(root1.left, root2.left)
+        root.right = mergeTrees(root1.right, root2.right)
+        return root
     }
 }
 
 fun main() {
     val r1 = TreeNode.createTestData("[1,3,2,5]")?.also { it.print() }
     val r2 = TreeNode.createTestData("[2,1,3,null,4,null,7]")?.also { it.print() }
-    Solution().mergeTrees(r1, r2)?.also { it.print() }
+    Solution().mergeTrees(r1, r2)?.also { it.print() } //
 }
