e2a5fe339b18f3504f998587b7d36cbafd8fde7b
wangxin21 <wangxin21@corp.netease.com>
2024-04-24 09:14:09 +0800

    second: 0238 update record.

--- src/main/kotlin/wang/relish/leetcode/second/_0238/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_0238/Solution.kt
@@ -1,8 +1,42 @@
 package wang.relish.leetcode.second._0238
 
 class Solution {
+    /**
+     * 官解解法一, 空间可以再优化掉一个数组
+     */
+    fun productExceptSelf1(nums: IntArray): IntArray {
+        val n = nums.size
+        val ans = IntArray(n) { 1 }
+        for (i in 1 until n) {
+            ans[i] = ans[i - 1] * nums[i - 1]
+        }
+        val ans2 = IntArray(n) { 1 }
+        for (i in n - 2 downTo 0) {
+            ans2[i] = ans2[i + 1] * nums[i + 1]
+        }
+        for (i in 0 until n) {
+            ans[i] *= ans2[i]
+        }
+        return ans
+    }
+
+    /**
+     * 双指针解法(前缀和 * 后缀和)
+     * 时间O(n), 空间O(1) // 返回值的ans不算空间
+     */
     fun productExceptSelf(nums: IntArray): IntArray {
-        return intArrayOf()
+        val n = nums.size
+        val ans = IntArray(n) { 1 }
+        var (i, j) = 0 to n - 1
+        var (beforeSum, afterSum) = 1 to 1
+        while (i < n && j >= 0) {
+            ans[i] *= beforeSum
+            ans[j] *= afterSum
+            beforeSum *= nums[i]
+            afterSum *= nums[j]
+            i++;j--
+        }
+        return ans
     }
 }
 
