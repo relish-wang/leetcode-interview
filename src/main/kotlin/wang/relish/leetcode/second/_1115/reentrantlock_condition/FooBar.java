c4d393fc59720d80eb613ab81c5c8bb4da3e775d
wangxin <wangxin21@corp.netease.com>
2024-04-25 15:21:50 +0800

    second: 1115 update record with ReentrantLock+Condition solution.

--- src/main/kotlin/wang/relish/leetcode/second/_1115/reentrantlock_condition/FooBar.java
+++ src/main/kotlin/wang/relish/leetcode/second/_1115/reentrantlock_condition/FooBar.java
@@ -1,5 +1,9 @@
 package wang.relish.leetcode.second._1115.reentrantlock_condition;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * https://leetcode.cn/problems/print-foobar-alternately/solutions/542996/duo-xian-cheng-liu-mai-shen-jian-ni-xue-d220n/?envType=featured-list&envId=Fw9n57OM?envType=featured-list&envId=Fw9n57OM
  * 1 LinkedBlockingQueue
@@ -17,17 +21,44 @@ class FooBar {
         this.n = n;
     }
 
+    final Lock lock = new ReentrantLock();
+    final Condition condition = lock.newCondition();
+
+    volatile boolean flag = true;
+
     public void foo(Runnable printFoo) throws InterruptedException {
 
         for (int i = 0; i < n; i++) {
-            printFoo.run();
+            lock.lock();
+            try {
+                while (!flag) {
+                    condition.await();
+                }
+                // printFoo.run() outputs "foo". Do not change or remove this line.
+                printFoo.run();
+
+            } finally {
+                condition.signal();
+                flag = false;
+                lock.unlock();
+            }
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
-
         for (int i = 0; i < n; i++) {
-            printBar.run();
+            lock.lock();
+            try {
+                while (flag) {
+                    condition.await();
+                }
+                // printBar.run() outputs "bar". Do not change or remove this line.
+                printBar.run();
+            } finally {
+                condition.signal();
+                flag = true;
+                lock.unlock();
+            }
         }
     }
 
