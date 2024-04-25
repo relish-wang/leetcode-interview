257dfa6f65d2a9e29b00d1affd88d5d6ba76337b
wangxin <wangxin21@corp.netease.com>
2024-04-25 11:49:16 +0800

    second: 1028 update record.

--- src/main/kotlin/wang/relish/leetcode/second/_1028/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/second/_1028/Solution.kt
@@ -1,7 +1,7 @@
 package wang.relish.leetcode.second._1028
 
 import struct.TreeNode
-import java.util.Stack
+import java.util.*
 import java.util.regex.Pattern
 
 /**
@@ -16,12 +16,52 @@ import java.util.regex.Pattern
  */
 class Solution {
     fun recoverFromPreorder(traversal: String): TreeNode? {
-        return null
+        val list = f(traversal)
+        val root = TreeNode(list[0].first)
+        var i = 1
+        val s = Stack<Pair<TreeNode, Int>>()
+        s.push(root to 0)
+        while (i < list.size) {
+            val (value, level) = list[i]
+            val (p, pLevel) = s.peek()
+            if (pLevel + 1 == level) {
+                val n = TreeNode(value)
+                if (p.left == null) {
+                    p.left = n
+                    s.push(n to level)
+                    i++
+                } else if (p.right == null) {
+                    p.right = n
+                    s.push(n to level)
+                    i++
+                } else {
+                    s.pop()
+                }
+            } else if (pLevel == level) {
+                s.pop()
+            } else {
+                s.pop()
+            }
+        }
+        return root
+    }
+
+    fun f(traversal: String): List<Pair<Int, Int>> { // value to level
+        val compile = Pattern.compile("-*\\d+")
+        val m = compile.matcher(traversal)
+        val ans = mutableListOf<Pair<Int, Int>>()
+        while (m.find()) {
+            val group = m.group()
+            var i = 0
+            while (group[i] == '-') i++
+            ans.add(group.substring(i).toInt() to i)
+        }
+        return ans
     }
 }
 
 fun main() {
-//    Solution().recoverFromPreorder("1-2--3--4-5--6--7")?.print()
-//    Solution().recoverFromPreorder("1-2--3---4-5--6---7")?.print()
+    Solution().recoverFromPreorder("1-2--3--4-5--6--7")?.print()
+    Solution().recoverFromPreorder("1-2--3---4-5--6---7")?.print()
     Solution().recoverFromPreorder("1-401--349---90--88")?.print()
 }
