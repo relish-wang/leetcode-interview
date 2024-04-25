aa28031b740cbc3ef6a4a0060d859e2b6eff6ee5
wangxin <wangxin21@corp.netease.com>
2024-04-25 14:30:55 +0800

    second: 1115 update record with BlockingQueue solution.

--- src/main/kotlin/wang/relish/leetcode/second/_1115/linkedblockingqueue/FooBar.java
+++ src/main/kotlin/wang/relish/leetcode/second/_1115/linkedblockingqueue/FooBar.java
@@ -1,5 +1,7 @@
 package wang.relish.leetcode.second._1115.linkedblockingqueue;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
 /**
  * https://leetcode.cn/problems/print-foobar-alternately/solutions/542996/duo-xian-cheng-liu-mai-shen-jian-ni-xue-d220n/?envType=featured-list&envId=Fw9n57OM?envType=featured-list&envId=Fw9n57OM
  * 【1】 LinkedBlockingQueue
@@ -17,21 +19,26 @@ class FooBar {
         this.n = n;
     }
 
+    ArrayBlockingQueue<Integer> foo = new ArrayBlockingQueue<>(1);
+    ArrayBlockingQueue<Integer> bar = new ArrayBlockingQueue<>(1);
+
     public void foo(Runnable printFoo) throws InterruptedException {
 
         for (int i = 0; i < n; i++) {
-
+            foo.offer(i);
             // printFoo.run() outputs "foo". Do not change or remove this line.
-            printFoo.run(); // 打印foo
+            printFoo.run();
+            bar.take();
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
 
         for (int i = 0; i < n; i++) {
-
+            foo.take();
             // printBar.run() outputs "bar". Do not change or remove this line.
-            printBar.run(); // 打印"bar"
+            printBar.run();
+            bar.offer(i);
         }
     }
 
