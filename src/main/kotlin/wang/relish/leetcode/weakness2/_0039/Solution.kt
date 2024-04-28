d0e697a04c1df6447c373bb1a5dc0a84e2db6010
wangxin21 <wangxin21@corp.netease.com>
2024-04-28 21:59:42 +0800

    weakness2: 0039 10:51

--- src/main/kotlin/wang/relish/leetcode/weakness2/_0039/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/weakness2/_0039/Solution.kt
@@ -1,10 +1,50 @@
 package wang.relish.leetcode.weakness2._0039
 
 class Solution {
-
-    val ans = mutableListOf<List<Int>>()
     fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
-        return listOf()
+        candidates.sort()
+        ff(candidates, target, 0, mutableListOf())
+        return res
+    }
+
+    val res = mutableListOf<List<Int>>()
+
+    fun ff(candidates: IntArray, target: Int, index: Int, list: MutableList<Int>) {
+        if (target == 0) {
+            res.add(list.toList())
+            return
+        }
+        for (i in index until candidates.size) {
+            if (candidates[i] > target) break
+            list.add(candidates[i])
+            ff(candidates, target - candidates[i], i, list)
+            list.removeLast()
+        }
+    }
+
+
+    val ans = hashSetOf<MutableList<Int>>()
+    fun combinationSum1(candidates: IntArray, target: Int): List<List<Int>> {
+        ans.clear()
+        candidates.sort()
+        f(candidates, target, 0, 0, mutableListOf())
+        return ans.toList()
+    }
+
+    fun f(candidates: IntArray, target: Int, index: Int, sum: Int, list: MutableList<Int>) {
+        if (index >= candidates.size) return
+        val add = candidates[index]
+        val newSum = add + sum
+        if (newSum > target) {
+            return
+        }
+        if (newSum == target) {
+            ans.add(ArrayList(list).apply { add(add) })
+            return
+        }
+        f(candidates, target, index, newSum, ArrayList(list).apply { add(add) }) // 加进sum, 但不加index
+        f(candidates, target, index + 1, sum, ArrayList(list)) // 啥也不加就是玩
+        f(candidates, target, index + 1, newSum, ArrayList(list).apply { add(add) }) // 加进sum, 也加index
     }
 }
 
