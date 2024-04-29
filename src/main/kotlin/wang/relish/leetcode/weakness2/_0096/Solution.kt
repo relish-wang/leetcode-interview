f8a65da61aecf80f58ecd1d0db1e05bb00b0e7a2
wangxin21 <wangxin21@corp.netease.com>
2024-04-29 14:18:15 +0800

    weakness2: 0096 06:28

--- src/main/kotlin/wang/relish/leetcode/weakness2/_0096/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/weakness2/_0096/Solution.kt
@@ -1,9 +1,17 @@
 package wang.relish.leetcode.weakness2._0096
 
 class Solution {
-
     fun numTrees(n: Int): Int {
-        return 0
+        val dp = IntArray(n + 1)
+        dp[0] = 1
+        if (n == 0) return 1
+        dp[1] = 1
+        for (i in 2..n) {
+            for (j in 0 until i) {
+                dp[i] += dp[j] * dp[i - j - 1]
+            }
+        }
+        return dp[n]
     }
 }
 
@@ -13,4 +21,5 @@ fun main() {
     println(Solution().numTrees(3)) // 5
     println(Solution().numTrees(4)) // 14
     println(Solution().numTrees(5)) // 42
+    println(Solution().numTrees(19)) // 1767263190
 }
