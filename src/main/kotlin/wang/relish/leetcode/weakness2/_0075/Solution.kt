b915fcdfcd8b96921fcd37b0553012f28c55867c
wangxin21 <wangxin21@corp.netease.com>
2024-04-29 10:45:36 +0800

    weakness2: 0075 08:48

--- src/main/kotlin/wang/relish/leetcode/weakness2/_0075/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/weakness2/_0075/Solution.kt
@@ -2,14 +2,39 @@ package wang.relish.leetcode.weakness2._0075
 
 class Solution {
     fun sortColors(nums: IntArray): Unit {
+        var p0 = 0
+        var p1 = 0
+        for (i in 0 until nums.size) {
+            if (nums[i] == 0) {
+                swap(nums, i, p0)
+                if (nums[i] == 1) {
+                    swap(nums, i, p1)
+//                    p1++
+                }
+                p0++
+                p1++
+            } else if (nums[i] == 1) {
+                swap(nums, i, p1)
+                p1++
+            }
+        }
+    }
+
+    fun swap(nums: IntArray, i: Int, j: Int) {
+        if (i !in nums.indices || j !in nums.indices) return
+        val t = nums[i]
+        nums[i] = nums[j]
+        nums[j] = t
     }
 }
 
 fun main() {
-    val nums = intArrayOf(2, 0, 2, 1, 1, 0)
+//    val nums = intArrayOf(2, 0, 2, 1, 1, 0)
 //    val nums = intArrayOf(2, 0, 1)
 //    val nums = intArrayOf(2, 1, 2)
-//    val nums = intArrayOf(2)
+//    val nums = intArrayOf(1)
+//    val nums = intArrayOf(1, 0, 0)
+    val nums = intArrayOf(1,2,0,0)
 //    val nums = intArrayOf(0, 2)
 //    val nums = intArrayOf(0, 2, 2, 2, 0, 2, 1, 1)
 
