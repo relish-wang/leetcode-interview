7e88dae40d7dd81e01cfbc17260af87c366e11b8
relish-wang <relish.wang@gmail.com>
2024-05-02 12:56:09 +0800

    weakness2: 0209 02:36.

--- src/main/kotlin/wang/relish/leetcode/weakness2/_0209/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/weakness2/_0209/Solution.kt
@@ -3,32 +3,16 @@ package wang.relish.leetcode.weakness2._0209
 import kotlin.math.min
 
 class Solution {
-    /**
-     * https://leetcode.cn/problems/minimum-size-subarray-sum/?envType=featured-list&envId=Fw9n57OM?envType=featured-list&envId=Fw9n57OM
-     */
     fun minSubArrayLen(target: Int, nums: IntArray): Int {
-        val n = nums.size
-        if (n == 1) {
-            return if (nums[0] >= target) 1 else 0
-        }
+        var l = 0
+        var r = 0
+        var sum = 0
         var min = Int.MAX_VALUE
-        var left = 0
-        var right = 1
-        var sum = nums[0]
-        while (left < right && right < n) { // 闭区间
-            while (right < n && sum < target) {
-                sum += nums[right]
-                right++
-            }
-            if (sum >= target) {
-                min = minOf(right - left, min)
-            }
-            while (left < right && sum >= target) {
-                sum -= nums[left]
-                left++
-            }
-            if (left - 1 >= 0 && sum + nums[left - 1] >= target) {
-                min = minOf(right - left + 1, min)
+        while (r < nums.size) {
+            sum += nums[r++]
+            while (sum >= target) {
+                min = min(r - l, min)
+                sum -= nums[l++]
             }
         }
         return min.takeIf { it != Int.MAX_VALUE } ?: 0
