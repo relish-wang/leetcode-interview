263e21f8dc8d63fdc9a8a315193b381d3fcf0d1a
wangxin <wangxin21@corp.netease.com>
2024-05-04 09:37:18 +0800

    weakness2: 0215 heapsort 15:47

--- src/main/kotlin/wang/relish/leetcode/weakness2/_0215/heapsort/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/weakness2/_0215/heapsort/Solution.kt
@@ -1,40 +1,46 @@
 package wang.relish.leetcode.weakness2._0215.heapsort
 
 class Solution {
-
-      fun findKthLargest(nums: IntArray, k: Int): Int {
+    /**
+     * 堆排序解法
+     * 1 <= k <= nums.length <= 105
+     *
+     * -10<sup>4</sup> <= nums\[i] <= 10<sup>4</sup>
+     */
+    fun findKthLargest(nums: IntArray, k: Int): Int {
         val n = nums.size
         for (i in n / 2 downTo 0) {
-            heapSort(nums, i, n) // 建堆
+            heapSort(nums, i, n)
         }
         for (i in n - 1 downTo n - k + 1) {
-            swap(nums, 0, i) // 把第i个节点换到堆顶？
-            heapSort(nums, 0, i)// 排序
+            swap(nums, 0, i)
+            heapSort(nums, 0, i)
         }
         return nums[0]
     }
 
-    fun heapSort(arr: IntArray, index: Int, size: Int) {
-        val left = (index shl 1) + 1 // 左子树节点index
-        var s = index // 当前父节点值index
-        if (left < size && arr[left] > arr[s]) {
-            s = left // 左子树节点值更大
+    fun heapSort(nums: IntArray, index: Int, size: Int) {
+        val left = (index shl 1) + 1
+        var s = index
+        if (left < size && nums[left] > nums[s]) {
+            s = left
         }
-        val right = left + 1 // 右子树节点index
-        if (right < size && arr[right] > arr[s]) {
-            s = right // 右子树节点值更大
+        val right = left + 1
+        if (right < size && nums[right] > nums[s]) {
+            s = right
         }
-        if (s == index) return // 当前父节点值最大
-        swap(arr, index, s) // 把最大值换到父节点位置
-        heapSort(arr, s, size) // 持续把刚才被换的子节点进行交换, 直到s==index
+        if (s == index) return
+        swap(nums, index, s)
+        heapSort(nums, s, size)
     }
 
-    fun swap(nums: IntArray, a: Int, b: Int) {
-        val t = nums[a]
-        nums[a] = nums[b]
-        nums[b] = t
+    fun swap(nums: IntArray, i: Int, j: Int) {
+        nums[i] = nums[i] xor nums[j]
+        nums[j] = nums[j] xor nums[i]
+        nums[i] = nums[i] xor nums[j]
     }
 }
+
 fun main() {
     println(Solution().findKthLargest(intArrayOf(3, 2, 1, 5, 6, 4), 2)) // 5
     println(Solution().findKthLargest(intArrayOf(3, 2, 3, 1, 2, 4, 5, 5, 6), 4)) // 4
