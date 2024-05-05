d761f71d28b9aae7c74fb747ef3c113869f328b6
relish-wang <relish.wang@gmail.com>
2024-05-05 16:37:24 +0800

    weakness2: 0300 13:39.

--- src/main/kotlin/wang/relish/leetcode/weakness2/_0300/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/weakness2/_0300/Solution.kt
@@ -1,11 +1,33 @@
 package wang.relish.leetcode.weakness2._0300
 
+
 /**
  * https://leetcode.cn/problems/longest-increasing-subsequence/?envType=featured-list&envId=Fw9n57OM?envType=featured-list&envId=Fw9n57OM
  */
 class Solution {
     fun lengthOfLIS(nums: IntArray): Int {
-        return 0
+        val n = nums.size
+        val dp = mutableListOf<Int>()
+        dp.add(nums[0])
+        for (i in 1 until n) {
+            val last = dp.last()
+            val curr = nums[i]
+            if (last < curr) {
+                dp.add(curr)
+            } else if (last == curr) {
+                continue
+            } else {
+                // <editor-fold desc="这里可以优化为二分查找, 把O(n)优化为O(logn)">
+                var j = dp.size
+                do {
+                    j--
+                    if (j < 0) break
+                } while (dp[j] >= curr)
+                dp[j+1] = curr
+                // </editor-fold>
+            }
+        }
+        return dp.size
     }
 }
 
