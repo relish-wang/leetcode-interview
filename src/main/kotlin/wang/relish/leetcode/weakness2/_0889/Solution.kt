4faa86dcb41a7338d453ddd68655e3b9f0a180df
wangxin21 <wangxin21@corp.netease.com>
2024-05-09 08:55:37 +0800

    weakness2: 0889 14:00

--- src/main/kotlin/wang/relish/leetcode/weakness2/_0889/Solution.kt
+++ src/main/kotlin/wang/relish/leetcode/weakness2/_0889/Solution.kt
@@ -15,11 +15,39 @@ import struct.TreeNode
  * @see _105.bt
  */
 class Solution {
-
+    /**
+     * 应该整个indexMap提升效率
+     */
     fun constructFromPrePost(preorder: IntArray, postorder: IntArray): TreeNode? {
-        return null
+        val n = preorder.size
+        return bt(preorder, postorder, 0, n - 1, 0, n - 1)
+    }
+
+    fun bt(
+        preorder: IntArray,
+        postorder: IntArray,
+        preLeft: Int,
+        preRight: Int,
+        postLeft: Int,
+        postRight: Int
+    ): TreeNode? {
+        val n = preorder.size
+        val range = 0 until n
+        if (preLeft !in range || preRight !in range || postLeft !in range || postRight !in range || preLeft > preRight || postLeft > postRight) {
+            return null
+        }
+        val rootValue = preorder[preLeft]
+        val root = TreeNode(rootValue)
+        if (preLeft + 1 >= n || preLeft == preRight) return root
+        val indexL = postorder.indexOf(preorder[preLeft + 1])
+        if (indexL == -1) return root
+        val lenL = indexL - postLeft
+        root.left = bt(preorder, postorder, preLeft + 1, preLeft + 1 + lenL, postLeft, postLeft + lenL)
+        root.right = bt(preorder, postorder, preLeft + 1 + lenL + 1, preRight, postLeft + lenL + 1, postRight - 1)
+        return root
     }
 }
+
 
 fun main() {
     // preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1]
